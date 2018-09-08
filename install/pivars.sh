58d42a460fd33f1d1ea82b5cc8698a16ec62235f
julianes10 <julian.espinosa.santos@ericsson.com>
2018-09-09 00:08:50 +0200

    adding kodiaggent config and deploy stuff

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 PROJECT_NAME=pipapi
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-SERVICES_LIST=simpleDHTtracker
+SERVICES_LIST="simpleDHTtracker kodiAgent"
 PI_USER=pi
-PI_IPNAME=pi3papa
+PI_IPNAME=pi2
 PI_PORT=22
 
 
