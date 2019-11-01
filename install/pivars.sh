73d26ddac3ff29c552b965097d6f733ed87cb121
julianes10 <julianes10@yahoo.com>
2019-11-01 21:32:00 +0100

    New pipapi use a picam

--- install/pivars.sh
+++ install/pivars.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 PROJECT_NAME=pipapi
 DEPLOY_FOLDER=/opt/$PROJECT_NAME
-#SERVICES_LIST="simpleDHTtracker kodiAgent"
-SERVICES_LIST="kodiAgent"
+#SERVICES_LIST="simpleDHTtracker kodiAgent webStreamingAgent"
+SERVICES_LIST="simpleDHTtracker webStreamingAgent telegramBOT mjpg-streamer  picam"
 PI_USER=pi
-#PI_IPNAME=ciriacostar.ddns.net
+
 #PI_PORT=5000
-PI_IPNAME=pi2
+PI_IPNAME=192.168.1.133
 PI_PORT=22
 
 
